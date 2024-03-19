6b758293d440f444acbede323e94cdbdfab20b48
HomareYamamoto <159757552+HomareYamamoto@users.noreply.github.com>
2024-03-19 14:38:23 +0000

    [課題3]ユーザー一覧機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,14 +4,16 @@ class BooksController < ApplicationController
 
   def create
     @book=Book.new(book_params)
+    @book.user_id=current_user.id
     if @book.save
-      flash[:notice] = "Book was successfully created."
+    flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books=Book.all
       render :index
     end
   end
+
 
   def index
     @book=Book.new
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get 'home/about' => "homes#about"
-  resources :books, only: [:create,:edit, :index, :show, :destroy]
+  resources :books, only: [:create,:edit, :index, :show, :destroy, :update]
+  resources :users, only: [:show, :edit, :update, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
